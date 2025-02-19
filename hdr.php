78009641f61b621a327b2471dca59d55b113d7ca
Alysson <alysson.gabriel61@gmail.com>
2025-02-19 12:38:02 -0400

    tira margem do header e menu e aplica padding na main

---
+++ hdr.php
@@ -0,0 +1,36 @@
+<!--element which is going to render-->
+<div class="control-container">
+  <header id="appbar">
+    <button id="defaultButtonMenu"></button>
+    <span class="regular" style="margin:0 5px">Nuvio</span>
+    <div class="e-appbar-spacer"></div>
+    <button id="defaultButtonLogin"></button>
+  </header>
+</div>
+
+<style>
+
+    .control-container{
+        margin: 0;
+        padding: 0;
+    }
+
+    #appbar{
+        margin: 0;
+        padding: 0;
+    }
+
+</style>
+
+<script>
+  var appbarObj = new ej.navigations.AppBar({
+    colorMode: 'Primary'
+  });
+  appbarObj.appendTo("#appbar");
+
+  var defaultButtonMenuObj = new ej.buttons.Button({ cssClass: 'e-inherit', iconCss: 'e-icons e-menu' });
+  defaultButtonMenuObj.appendTo('#defaultButtonMenu');
+
+  var defaultButtonLoginObj = new ej.buttons.Button({ cssClass: 'e-inherit', content: 'Logout' });
+  defaultButtonLoginObj.appendTo('#defaultButtonLogin');
+</script>
--- index.php
+++ index.php
@@ -5,35 +5,48 @@
     <link href="resources/styles/material.css" rel="stylesheet" type="text/css"/>
     <script src="resources/scripts/ej2.min.js" type="text/javascript"></script>
     <script>ej.base.registerLicense('Ngo9BigBOggjHTQxAR8/V1NMaF1cXmhKYVF2WmFZfVtgc19DYFZQR2YuP1ZhSXxWdkdiUX1cdX1UQmBaVEw=')</script>
+    <style>
+        *{
+            padding: 0;
+            margin: 0;
+        }
+
+        main{
+            padding: 10px;
+        }
+    </style>
 </head>
 <body>
 
-    <header> <?php include('header.php') ?> </header>
+    <head> <?php include('hdr.php') ?> </head>
+    <div> <?php include('header.php') ?> </div>
+    
+    <main>
+        <h2>Componente Grid</h2>
 
-    <h2>Componente Grid</h2>
-
-      <!-- Add the HTML <div> element  -->
-      <div id="element">
-             <div class="e-tab-header">
-            <div> Grid 2 </div>
-            <div> Grid 3 </div>
-        </div>
-        <div class="e-content">
-            <div id="Grid">
-                <script src="es5-datasource.js" type="text/javascript"></script>
-                <script src="grid1.js"></script>
+            <!-- Add the HTML <div> element  -->
+            <div id="element">
+                <div class="e-tab-header">
+                <div> Grid 2 </div>
+                <div> Grid 3 </div>
             </div>
-            <div id="Grid2">
-                <script src="grid2.js"></script>
+            <div class="e-content">
+                <div id="Grid">
+                    <script src="es5-datasource.js" type="text/javascript"></script>
+                    <script src="grid1.js"></script>
+                </div>
+                <div id="Grid2">
+                    <script src="grid2.js"></script>
+                </div>
             </div>
-        </div>
-         </div>
+            </div>
 
-        <script>
-          var tabObj = new ej.navigations.Tab();
+            <script>
+            var tabObj = new ej.navigations.Tab();
 
-          //Render initialized Tab component
-          tabObj.appendTo('#element');
-        </script>
+            //Render initialized Tab component
+            tabObj.appendTo('#element');
+            </script>
+        </main>
 </body>
 </html>
