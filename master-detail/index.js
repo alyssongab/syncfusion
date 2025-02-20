5f6e0383e4107082869a03122e985b605bb9b377
Alysson <alysson.gabriel61@gmail.com>
2025-02-20 09:56:52 -0400

    redimensionamento personalizado

--- master-detail/index.js
+++ master-detail/index.js
@@ -7,12 +7,13 @@ ej.base.enableRipple(window.ripple)
     
     var mastergrid = new ej.grids.Grid({
         dataSource: masterdata,
+        allowResizing: true,
         selectedRowIndex: 1,
         columns: [
             { field: 'ContactName', headerText: 'Customer Name', width: 150,},
             { field: 'CompanyName', headerText: 'Company Name', width: 150 },
             { field: 'Address', headerText: 'Address', width: 150 },
-            { field: 'Country', headerText: 'Country', width: 130 }
+            { field: 'Country', headerText: 'Country', allowResizing: false, width: 130 }
         ],
         rowSelected: rowSelected,
     });
@@ -28,13 +29,13 @@ ej.base.enableRipple(window.ripple)
 
     var grid = new ej.grids.Grid({
         allowSelection: false,
-        
+        allowResizing: true,
         columns: [
             { field: 'OrderID', headerText: 'Order ID', width: 100, textAlign: 'Right' },
             { field: 'Freight', headerText: 'Freight', width: 100, format: 'C2', type:'number' },
             { field: 'ShipName', headerText: 'Ship Name', width: 200},
             { field: 'ShipCountry', headerText: 'Ship Country', width: 150 },
-            { field: 'ShipAddress', headerText: 'Ship Address', width: 200 },
+            { field: 'ShipAddress', headerText: 'Ship Address', allowResizing: false, width: 200 },
         ]
     });
     grid.appendTo('#DetailGrid');
